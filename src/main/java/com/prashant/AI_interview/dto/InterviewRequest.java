209b9d5d96f9a7921832b91b5504cc3971fe480a
PRASHANT PATHAK <prashant.pathak112@gmail.com>
2026-05-09 13:29:18 +0530

    Update InterviewRequest.java

--- src/main/java/com/prashant/AI_interview/dto/InterviewRequest.java
+++ src/main/java/com/prashant/AI_interview/dto/InterviewRequest.java
@@ -3,14 +3,18 @@ package com.prashant.AI_interview.dto;
 public class InterviewRequest {
 
     private String role;
+    private String question;
     private String answer;
+    private int questionNumber;
 
     public InterviewRequest() {
     }
 
-    public InterviewRequest(String role, String answer) {
+    public InterviewRequest(String role, String question, String answer, int questionNumber) {
         this.role = role;
+        this.question = question;
         this.answer = answer;
+        this.questionNumber = questionNumber;
     }
 
     public String getRole() {
@@ -21,11 +25,27 @@ public class InterviewRequest {
         this.role = role;
     }
 
+    public String getQuestion() {
+        return question;
+    }
+
+    public void setQuestion(String question) {
+        this.question = question;
+    }
+
     public String getAnswer() {
         return answer;
     }
 
     public void setAnswer(String answer) {
         this.answer = answer;
+    }
+
+    public int getQuestionNumber() {
+        return questionNumber;
+    }
+
+    public void setQuestionNumber(int questionNumber) {
+        this.questionNumber = questionNumber;
     }
 }
